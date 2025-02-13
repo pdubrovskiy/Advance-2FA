1aba11179f91bd4381846308539f39fa49b8da41
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2025-02-13 22:12:19 +0300

    F-1: code formatted

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  app.use(cookieParser(config.getOrThrow<string>('COOKIES_SECRET')));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    })
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
